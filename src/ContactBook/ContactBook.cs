941ed3f35347c676f54dae726d0379a5a15ac3d9
Yedriel Laureano <y.laureano2006@gmail.com>
2026-05-01 07:10:04 -0400

    Add: ShowWelcomeScreen

---
+++ src/ContactBook/ContactBook.cs
@@ -0,0 +1,97 @@
+namespace ContactBook;
+
+public class ContactBook
+{
+    public const string NEXT_PAGE = "+";
+    public const string PREV_PAGE = "-";
+    public const string GOTO_PAGE = "G";
+    public const string PAGE_SIZE = "S";
+    public const string CREATE_CONTACT = "C";
+    public const string REVIEW_CONTACT = "R";
+    public const string UDATE_CONTACT = "U";
+    public const string DELETE_CONTACT = "D";
+    public const string FIND_CONTACTS = "F";
+    public const string ORDER_CONTACTS = "O";
+    public const string DEDUPLICATE_CONTACTS = "M";
+    public const string EXIT = "X";
+
+    public readonly string[] COMMANDS = new string[]
+    {
+     NEXT_PAGE, PREV_PAGE, GOTO_PAGE, PAGE_SIZE, CREATE_CONTACT, 
+     REVIEW_CONTACT, UDATE_CONTACT, DELETE_CONTACT, FIND_CONTACTS, 
+     ORDER_CONTACTS, DEDUPLICATE_CONTACTS, EXIT,
+    };
+
+    public ContactBook()
+    {
+        
+    }
+
+    public void Start()
+    {
+        ShowWelcomeScreen();
+
+        string input;
+        do
+        {
+            ShowContacts();
+
+            do
+            {
+                ShowInputOptions();
+                input = GetInput();
+            }
+            while(!IsValidInput(input));
+
+            ProcessInput(input);
+        }
+        while(!ConfirmExit());
+    }
+
+    private void ShowWelcomeScreen()
+    {
+        Console.WriteLine("Welcome to Simple Contact Book!");
+        PressEnterContinue();
+    }
+
+    private void ShowContacts()
+    {
+        
+    }
+
+    private void ShowInputOptions()
+    {
+        
+    }
+
+    private string GetInput()
+    {
+        return "";
+    }
+
+    private bool IsValidInput(string input)
+    {
+        return true;
+    }
+
+    private void ProcessInput(string input)
+    {
+        
+    }
+
+    private bool ConfirmExit()
+    {
+        return true;
+    }
+
+    private void ShowExitScreen()
+    {
+        
+    }
+
+    private void PressEnterContinue()
+    {
+        Console.Write("Press ENTER to continue.");
+        while(Console.ReadKey(true).Key != ConsoleKey.Enter);
+    }
+}
