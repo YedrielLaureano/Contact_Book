d8a721bac7ea81c8ac6415db2788e15eb0415e0f
Yedriel Laureano <y.laureano2006@gmail.com>
2026-05-04 08:56:10 -0400

    Add: PageSize

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -228,7 +228,14 @@ public class ContactBook
 
     private void PageSize()
     {
-        
+        PageSize(ref page, ref size);
+    }
+
+    private void PageSize(ref int page, ref int size)
+    {
+        int max = Console.WindowHeight - 10;
+        size = GetInt("Enter page size", 1, max);
+        page = 1;
     }
 
     private void CreateContact()
