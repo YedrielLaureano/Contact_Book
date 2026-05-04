b30ebb71546ce7039154ffbed5a0f52beca07b7a
Yedriel Laureano <y.laureano2006@gmail.com>
2026-05-04 08:49:49 -0400

    Add: GotoPage

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -218,7 +218,12 @@ public class ContactBook
 
     private void GotoPage()
     {
-        
+        GotoPage(allContacts, ref page, size);
+    }
+
+    private void GotoPage(List<Contact> contacts, ref int page, int size)
+    {
+        page = GetInt("Enter page", 1, PageCount(contacts, size));
     }
 
     private void PageSize()
@@ -264,6 +269,24 @@ public class ContactBook
     private void Exit()
     {
         isExit = true;
+    }
+
+    private int GetInt(string prompt, int min, int max)
+    {
+        string options = $"{min}-{max}";
+
+        Console.Write(prompt + $" [{options}] ");
+        string answer = Console.ReadLine()!;
+        int value;
+
+        while(!int.TryParse(answer, out value) || value < min || value > max)
+        {
+            Console.WriteLine("ERROR: Invalid option. Please try again.");
+            Console.Write(prompt + $" [{options}] ");
+            answer = Console.ReadLine()!;
+        }
+
+        return value;
     }
 
     private string GetOption(string prompt, string[] validOptions, string defaultOption)
